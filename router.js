33ff9c7b0f157d81cfc616bca50b5162a0583e55
J. TURNER <turner.j6@gmail.com>
2015-03-06 16:03:40 -0800

    Update files.

--- router.js
+++ router.js
@@ -1,6 +1,8 @@
 Email.Router.map(function() {
   this.resource('application');
   this.resource('home', { path: '/' });
+  this.resource('new-user');
   this.resource('users');
   this.resource('advice');
+  this.resource('send-message');
 });
