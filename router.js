c9fd666c5f6a7025d09f6775908c5e59da20d09b
J. TURNER <turner.j6@gmail.com>
2015-03-07 10:36:16 -0800

    Add 'user' to router.

--- router.js
+++ router.js
@@ -2,6 +2,7 @@ Email.Router.map(function() {
   this.resource('application');
   this.resource('home', { path: '/' });
   this.resource('new-user');
+  this.resource('user');
   this.resource('users');
   this.resource('advice');
   this.resource('send-message');
